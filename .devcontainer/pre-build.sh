c7d096e5d292a131fbcb50f45f2092d43e8a5edb
Mason Malone <mmalone@adobe.com>
2025-11-11 15:43:23 -0500

    Update

--- .devcontainer/pre-build.sh
+++ .devcontainer/pre-build.sh
@@ -2,11 +2,8 @@
 set -eux
 
 # create cluster using the minimum tested Kubernetes version
-k3d cluster get k3s-default || k3d cluster create --image "rancher/k3s:v1.30.4-k3s1" --wait
+k3d cluster get k3s-default || k3d cluster create --image "rancher/k3s:${K8S_VERSION}-k3s1" --wait
 k3d kubeconfig merge --kubeconfig-merge-default
 kubectl cluster-info
 
-# Make sure go path is owned by vscode
-#sudo chown vscode:vscode /home/vscode/go || true
-#sudo chown vscode:vscode /home/vscode/go/src || true
-#sudo chown vscode:vscode /home/vscode/go/src/github.com || true
+go install github.com/mattn/goreman@latest
