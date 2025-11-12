4dad0c78e19376f778b1528b2a01e90e0c0895e8
Mason Malone <mmalone@adobe.com>
2025-11-12 09:01:18 -0500

    Updates

--- .devcontainer/pre-build.sh
+++ .devcontainer/pre-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -eux
 
-# create cluster using the minimum tested Kubernetes version
+# create cluster
 k3d cluster get k3s-default || k3d cluster create --image "rancher/k3s:${K8S_VERSION}-k3s1" --wait
 k3d kubeconfig merge --kubeconfig-merge-default
 kubectl cluster-info
 
+# Install Goreman: https://github.com/mattn/goreman#installation
 go install github.com/mattn/goreman@latest
+make install-tools-local
