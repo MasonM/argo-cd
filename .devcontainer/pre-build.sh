08a231fce7f86a2f4d40b6798c70f2d5e0b4bb35
Mason Malone <mmalone@adobe.com>
2025-11-12 16:14:43 -0500

    Create registry

--- .devcontainer/pre-build.sh
+++ .devcontainer/pre-build.sh
@@ -2,7 +2,7 @@
 set -eux
 
 # create cluster
-k3d cluster get k3s-default || k3d cluster create --image "rancher/k3s:${K8S_VERSION}-k3s1" --wait
+k3d cluster get k3s-default || k3d cluster create k3s-default --image "rancher/k3s:${K8S_VERSION}-k3s1" --wait --registry-create k3s-registry
 k3d kubeconfig merge --kubeconfig-merge-default
 kubectl cluster-info
 
